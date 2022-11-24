ea973aa22ace74f7e99099ad8c31ec1b3ab5c651
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-11-24 13:24:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,71 @@
+�
+
+3|{c;��d�ddlZddlmZmZddlmZddlZddlZddlZddlZddlZddl	Z	dZ
+e��ejZej
+ZejZejZejZejZeeeeegZd�Zd�Z	e��e��eedzez��eed	zez��eed
+zez��eed����Zedkr�ed
+��ejd��ejd��ejd��ejd��ejd��ejd��ejd��ejd��ejd��ed��ed��ed��edkre��e��e����C)�N)�init�Fore)�sleepz	@notoscamc	�~�tjd��tdt�dt�dt
+�d���dS)N�clear�
+u�
+
+                     
+               ┏┳┳━┳┳━━┳━━┳━┳━┳━┳━━┓╋╋╋╋┏━━━┓
+               ┃┃┃┃┃┣┓┏┫┏┓┃┃┃┃┃┳┻┓┓┃┏━┳━┫┏━┓┃
+               ┃┃┃┃┃┃┃┃┃┣┫┃┃┃┃┃┻┳┻┛┃┗┓┃┏┻┛┏┛┃
+               ┗━┻┻━┛┗┛┗┛┗┻┻━┻┻━┻━━┛╋┗━┛┏┓┗┓┃uj
+               ╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┃┗━┛┃z
+
+)�os�system�print�cy�ye�r���6/storage/emulated/0/Download/Telegram/untamed/setup.py�bannerrsg���I�g����	�
+��
+�
+�km�
+�
+�kl�
+�
+�
+�
+�
+�
+�
+�
+rc�z�tjdkrtjd��dStjd��dS)N�nt�clsr)r	�namer
+rrr�clrr"s<��	�w�$���
+�	�%������
+�	�'�����rTzChoose an Option:z            [1] Setup Scriptz            [2] Exitz
+ Enter your choice: �z[+] Installing requierments ...zpip install telethonzpip install coloramazpip install requestszpip install richzpip install newthonzpip install pyrogramzpip install tgcryptozpip install pytzzpip install IP2Locationz[+] setup complete !z[+] now you can run any tool !z"
+ Press enter to goto main menu...�) r	�coloramarr�timer�csv�random�pickle�sys�scam�RESET�n�
+LIGHTGREEN_EX�lg�REDr�WHITE�w�CYANr�YELLOWr
+�colorsrrr�int�input�ar
+�exitrrr�<module>r/s���	�	�	�	���������������
+�
+�
+�
+�����
+�
+�
+�
+�	�	�	�	�
+�
+�
+�
+�
+�
+�
+�
+���������J��	����H���J��	�Y��	�[��
+�a��B��	��������
+��C�E�E�E�
+�F�H�H�H�	�E�"�
+ �
+ ��
+"�#�#�#�	�E�"�
++�
++�A�
+-�.�.�.�	�E�"�
+#�
+#�A�
+%�&�&�&���E�E�*�+�+�,�,�A��A�v�5��u�
+.�/�/�/��r�y�'�(�(�(��r�y�'�(�(�(��r�y�'�(�(�(��r�y�#�$�$�$��r�y�&�'�'�'��r�y�'�(�(�(��r�y�'�(�(�(��r�y�#�$�$�$��r�y�*�+�+�+��u�
+#�$�$�$��u�
+-�.�.�.��u�
+3�4�4�4��A�v�����������������7r
